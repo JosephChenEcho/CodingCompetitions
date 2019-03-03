487fd2b813a7a0366b38961e9333231ff0e22471
Yingwei Chen <josephchen6688@gmail.com>
2019-03-02 21:20:19 -0800

    Update kickalarm.py

--- 2019KickStart/Practice/kickalarm.py
+++ 2019KickStart/Practice/kickalarm.py
@@ -18,19 +18,47 @@ def buildA(asize,cd,e1e2,f,init):
         i = i + 1
     return retarr
 
+#calculate power
+def calpower(para, Aarray):
+    Asize = len(Aarray)
+    returnpower = 0
+    alen = 1
+    #print(para)
+    while alen <= Asize:
+        start = 0
+        while start + alen <= Asize :
+            idx = 0
+            while idx < alen:
+                returnpower += Aarray[idx+start]*para[idx]
+                idx = idx + 1
+#                print("idx=" + str(idx) + "start="+str(start)+"alen="+str(alen)+"Asize="+str(Asize))
+            start = start + 1
+            #print("idx=" + str(idx) + "start="+str(start)+"alen="+str(alen)+"Asize="+str(Asize))
+
+        alen = alen + 1
+    return returnpower
 #do loop for input
+
+
+
 i = 0
 while i < inputsize:
     k = int(numarr[i][1])
     aarr = buildA(int(numarr[i][0]),int(numarr[i][4]) + int(numarr[i][5]),int(numarr[i][6])+int(numarr[i][7]),int(numarr[i][8]),int(numarr[i][2])+int(numarr[i][3]))
     #debug
-    print(aarr)
+    #print(aarr)
     j = 0
     power = 0
+    para = [1]*len(aarr)
+    totalpower = 0
     while j < k:
-
-
+        m = 1
+        while m <= len(para):
+            para[m-1] *= m
+            m += 1
+        #print(calpower(para,aarr))
+        totalpower += calpower(para,aarr)
         j = j + 1
-    
+    print("Case #"+str(i+1)+": "+ str(totalpower%1000000007))
     i = i + 1
     
